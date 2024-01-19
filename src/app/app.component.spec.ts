c882cf611857700bbffbc44b32d7d8c119975a07
Adrián Rodríguez Fernández <adrian.rodriguezfernandez@telefonica.com>
2024-01-19 11:36:27 +0100

    failed test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -24,6 +24,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Introducción a Github Actions');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Introducció a Github Actions');
   });
 });
